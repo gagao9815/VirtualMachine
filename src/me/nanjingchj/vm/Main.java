78b6bb6e733ed49bc625fabcd7d10457ae726b10
nanjingchj <nanjingchj@gmail.com>
2019-11-05 17:44:48 -0500

    init

--- src/me/nanjingchj/vm/Main.java
+++ src/me/nanjingchj/vm/Main.java
@@ -5,19 +5,14 @@
  */
 package me.nanjingchj.vm;
 
-import java.io.*;
-import java.util.Arrays;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
-/**
- * @author njche
- */
 public class Main {
+    private static FileInputStream inputStream;
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         if (args.length != 1) {
             System.out.println("Please provide the input file");
             System.exit(0);
@@ -27,19 +22,20 @@ public class Main {
 
         // execute
         try {
-            FileInputStream inputStream = new FileInputStream(fileName);
+            inputStream = new FileInputStream(fileName);
             long fileSize = new File(fileName).length();
             byte[] bytes = new byte[(int) fileSize];
             inputStream.read(bytes);
-            System.out.println(Arrays.toString(bytes));
+            //System.out.println(Arrays.toString(bytes));
 
         } catch (IOException ex) {
             ex.printStackTrace();
+            System.exit(-1);
         }
     }
 
     public static byte nextByte() {
-
+        // TODO simulate a byte stream by returning the next byte to read
     }
 
 }
